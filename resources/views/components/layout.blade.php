2d4970d4be39708e114779d64a05c36c268ed6d3
JamieBurridge <xross_dev@proton.me>
2024-03-13 19:57:37 +0000

    Project setup

---
+++ resources/views/components/layout.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}" class="dark bg-base-300">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    @vite('resources/css/app.css')
+    <title>Cinema Ticket Booker</title>
+</head>
+
+<body>
+    {{$slot}}
+</body>
+
+</html>
