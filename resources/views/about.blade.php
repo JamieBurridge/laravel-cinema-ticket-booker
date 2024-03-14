f78ab76feb46781a61fcfbb8d9b8979e8287c36d
JamieBurridge <xross_dev@proton.me>
2024-03-14 16:27:30 +0000

    Add about page view

---
+++ resources/views/about.blade.php
@@ -0,0 +1,21 @@
+<x-layout>
+    <div class="flex flex-col gap-6 max-w-[600px] mx-auto">
+        <x-heading styles="pt-6">
+            About
+        </x-heading>
+
+        <p>Welcome to La-Cinema, where the magic of movies comes alive!</p>
+        
+        <p>La-Cinema is not just a theater; it's an experience. La-Cinema is a haven for movie enthusiasts seeking the ultimate cinematic journey.</p>
+        
+        <p>Our mission at La-Cinema is simple: to provide our guests with an unparalleled moviegoing experience. From the latest Hollywood blockbusters to timeless classics, our diverse selection of films ensures there's something for everyone.</p>
+        
+        <p>Step into our modern, state-of-the-art theaters and immerse yourself in the sights and sounds of the big screen. With crystal-clear visuals and immersive surround sound, every film comes to life at La-Cinema.</p>
+        
+        <p>But La-Cinema is more than just a place to watch movies—it's a community. Whether you're a solo movie buff or enjoying a night out with friends and family, La-Cinema offers a warm and welcoming environment for all.</p>
+        
+        <p>Hungry for snacks? Our concession stand has you covered with a variety of delicious treats and beverages to enjoy during the show. And our friendly staff is always on hand to ensure your visit is nothing short of extraordinary.</p>
+        
+        <p>Join us at La-Cinema and experience the magic of movies like never before. We can't wait to welcome you to our theater and make your movie night one to remember.</p>
+    </div>
+</x-layout>
