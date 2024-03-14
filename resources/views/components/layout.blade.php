d6bb7155d1a02fc8ee2f3d3e94f292f54e9196b3
JamieBurridge <xross_dev@proton.me>
2024-03-14 16:27:08 +0000

    Add navbar and footer

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -1,15 +1,21 @@
 <!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}" class="dark bg-base-300">
-
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}" class="dark bg-base-300 h-full">
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     @vite('resources/css/app.css')
     <title>Cinema Ticket Booker</title>
 </head>
+<body class="flex flex-col justify-between h-full">
+    <div>
+        <x-navbar />
+        
+        {{-- Main content --}}
+        <div class="p-4 {{ $styles ?? '' }}">
+            {{ $slot }}
+        </div>
+    </div>
 
-<body class="p-4">
-    {{$slot}}
+    <x-footer />
 </body>
-
 </html>
