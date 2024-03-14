52aa2b8342a58c402db0e5aefb6e4e40d0d9311f
JamieBurridge <xross_dev@proton.me>
2024-03-14 16:27:23 +0000

    Add homepage view

---
+++ resources/views/home.blade.php
@@ -0,0 +1,15 @@
+<x-layout styles="!p-0">
+    <div class="hero min-h-screen" style="background-image: url(https://images.unsplash.com/photo-1517604931442-7e0c8ed2963c?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D);">
+        <div class="hero-overlay bg-opacity-70"></div>
+        <div class="hero-content text-center text-neutral-content">
+            <div class="max-w-md">
+            <h1 class="mb-5 text-5xl font-bold">La-Cinema</h1>
+            <p class="mb-5">Catch the latest movies at the best prices. Get an authentick cinema experience with food and drinks included.</p>
+            
+            <a href="/movies">
+                <button class="btn btn-primary">See what's screening</button>
+            </a>
+            </div>
+        </div>
+    </div>
+</x-layout>
